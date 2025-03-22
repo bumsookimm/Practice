e15c139aa00570898d46d1170e3a3346d256d053
BUMOK\rhaen <rhaenfdlekd2@naver.com>
2025-03-22 15:42:38 +0900

    백엔드 - 게시판 controller 기본 세팅 진행 프론트엔드 - 게시판 메인 메뉴 사이드바 생성 중

--- demo/src/main/java/com/example/demo/controller/boardController.java
+++ demo/src/main/java/com/example/demo/controller/boardController.java
@@ -1,20 +1,47 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class boardController {
 
-	
-	@GetMapping("/")
-	private String main () {
-		
-		
-		
-		
-		
-		return"/main";
+	@GetMapping("/board")
+	private String board() {
+
+		return "/board";
 	}
-	
+
+	@GetMapping("/boardList")
+	private String boardList() {
+
+		return "/boardList";
+	}
+
+	@GetMapping("/boardView")
+	private String boardView() {
+
+		return "/boardView";
+	}
+
+	@PostMapping("/boardWrite")
+	private String boardWrite() {
+
+		return "/boardWrite";
+	}
+
+	@PostMapping("/boardModifty")
+	private String boardModifty() {
+
+		return "/boardModifty";
+	}
+
+	@DeleteMapping("/boardDelete")
+	private String boardDelete() {
+
+		return "/boardDelete";
+	}
+
 }
---
+++ demo/src/main/webapp/WEB-INF/views/board.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>board</title>
+<link rel="stylesheet" href="/static/css/board.css"="">
+</head>
+<body>
+<%@ include file="/WEB-INF/views/includes/header.jsp" %>
+
+	<nav class = sideBar>
+		<ul>
+			<li> <a href="boardList"> 게시판 목록</a> </li>
+			<li> <a href="boardWrite"> 글쓰기</a> </li>
+
+		</ul>
+	</nav>
+
+
+
+
+</body>
+</html>
